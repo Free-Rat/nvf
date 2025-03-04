f034c72e8a9048e4f3016c590ec4c4d5d3faba63
tomasz_lawicki <tomasz.lawicki@lstsoft.com.pl>
2025-03-04 12:47:32 +0100

    multisystem configuration

--- flake.nix
+++ flake.nix
@@ -2,25 +2,35 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nvf.url = "github:notashelf/nvf";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = {nixpkgs, ...} @ inputs: {
-    packages.x86_64-linux = {
-      # Set the default package to the wrapped instance of Neovim.
-      # This will allow running your Neovim configuration with
-      # `nix run` and in addition, sharing your configuration with
-      # other users in case your repository is public.
-      default =
-        (inputs.nvf.lib.neovimConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
-          modules = [
-            ./languages.nix
-            ./theme.nix
-            ./general.nix
-            ./utility.nix
-          ];
-        })
-        .neovim;
+  outputs = {
+    nixpkgs,
+    nvf,
+    flake-parts,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: {
+        packages = {
+          default =
+            (nvf.lib.neovimConfiguration {
+              pkgs = pkgs;
+              modules = [
+                ./languages.nix
+                ./theme.nix
+                ./general.nix
+                ./utility.nix
+              ];
+            })
+            .neovim;
+        };
+      };
     };
-  };
 }
